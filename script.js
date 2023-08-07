0979c938d72c13b881211afbde29e453efc04126
Jose Laurito <jigolaurito93@yahoo.com>
2023-08-07 04:06:58 -0500

    used while loop to loop the code

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
 var output = [];
 var count = 1
 
-function fizzBuzz() {
-    output.push(count)
-    count += 1;
+// function fizzBuzz() {
+    
+//     // If solution is divisible by 3 and 5
+//     if (count % 3 === 0 && count % 5 === 0){
+//         output.push('FizzBuzz');
 
-    console.log(output)
+//     // If solution is divisible by 5
+//     } else if (count % 5 === 0){
+//         output.push('Buzz')
+
+//     // If solution is divisible by 3
+//     } else if (count % 3 === 0){
+//         output.push('Fizz')
+//     } else {
+//         output.push(count)
+//     }
+//     count += 1;
+//     console.log(output)
+// }
+
+// fizzBuzz()
+
+//  USING WHILE LOOP
+
+
+function fizzBuzz() {
+    while (count <= 100){
+
+        
+        // If solution is divisible by 3 and 5
+    if (count % 3 === 0 && count % 5 === 0){
+        output.push('FizzBuzz');
+
+    // If solution is divisible by 5
+    } else if (count % 5 === 0){
+        output.push('Buzz')
+
+    // If solution is divisible by 3
+    } else if (count % 3 === 0){
+        output.push('Fizz')
+    } else {
+        output.push(count)
+    }
+    count += 1;
+    
+    } console.log(output)
 }
+
+fizzBuzz();
