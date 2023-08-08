9e65fde522d5eb7850743bd10983f606edb0cf71
Jose Laurito <jigolaurito93@yahoo.com>
2023-08-08 01:00:54 -0500

    used for loop for fizzbuzz

--- script.js
+++ script.js
@@ -26,27 +26,48 @@ var count = 1
 //  USING WHILE LOOP
 
 
-function fizzBuzz() {
-    while (count <= 100){
+// function fizzBuzz() {
+//     while (count <= 100){
 
         
-        // If solution is divisible by 3 and 5
-    if (count % 3 === 0 && count % 5 === 0){
-        output.push('FizzBuzz');
+//         // If solution is divisible by 3 and 5
+//     if (count % 3 === 0 && count % 5 === 0){
+//         output.push('FizzBuzz');
 
-    // If solution is divisible by 5
-    } else if (count % 5 === 0){
-        output.push('Buzz')
+//     // If solution is divisible by 5
+//     } else if (count % 5 === 0){
+//         output.push('Buzz')
 
-    // If solution is divisible by 3
-    } else if (count % 3 === 0){
-        output.push('Fizz')
-    } else {
-        output.push(count)
-    }
-    count += 1;
+//     // If solution is divisible by 3
+//     } else if (count % 3 === 0){
+//         output.push('Fizz')
+//     } else {
+//         output.push(count)
+//     }
+//     count += 1;
     
+//     } console.log(output)
+// }
+
+// fizzBuzz();
+
+
+// Using for loop
+
+var output = [];
+
+function fizzBuzz() {
+    for (var i = 1; i < 101; i++){
+        if (i % 3 === 0 && i % 5 === 0){
+            output.push('FizzBuzz');
+        } else if (i % 3 === 0){
+            output.push('Fizz')
+        } else if (i % 5 === 0){
+            output.push('Buzz')
+        } else {
+            output.push(i)
+        }
     } console.log(output)
 }
 
-fizzBuzz();
+fizzBuzz()
